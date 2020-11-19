2501dd0c5674edfa1d7daa9c6318fd1cf42eceed
Beatrice T <be@trice.codes>
2020-11-18 22:05:29 -0800

    Added basic product CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,38 @@ class ProductsController < ApplicationController
     @products = Product.all
 
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      redirect_to products_path
+      return
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_parms)
+
+    if @product.save
+      redirect_to product_path(@product.id)
+      return
+    else
+      @product.errors.each do |type, err|
+      flash[type] = err
+      end
+      render :new
+    end
+  end
+
+  
+
+
+
+    end
+  end
 end
