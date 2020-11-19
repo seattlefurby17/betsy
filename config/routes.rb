6ca9594140298c76bc409f82adaa0ee80b15d736
Ida <ida.goitom@yahoo.com>
2020-11-18 17:42:42 -0800

    added merchant and product views and index controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'homepages/index'
-  get 'order_items/index'
-  get 'orders/index'
-  get 'products/index'
+
+  root to: "homepages#index"
+  resources :homepages
+  resources :merchants
+  resources :order_items
+  resources :orders
+  resources :products
+  # get 'homepages/index'
+  # get 'order_items/index'
+  # get 'orders/index'
+  # get 'products/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
