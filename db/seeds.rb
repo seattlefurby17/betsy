d7d1861245c1a88a02df1d41781af1c83d4d0689
Beatrice T <be@trice.codes>
2020-11-18 17:10:46 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+merchants = Merchant.create!([{username: "coolperson", email: "coolperson@cool.com", provider:
+    "github", uid: "832943"}])
+products = Product.create!([{ name: "Furby", description: "A toy from the 90s", price: 9.99,
+                              stock: 200, merchant: merchants.first}])
