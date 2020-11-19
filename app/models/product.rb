df59510a762502592b85ce7e0603452b0e7187eb
Ida <ida.goitom@yahoo.com>
2020-11-19 10:43:31 -0800

    updated product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   belongs_to :merchant
   has_many :order_items
   validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
 end
