fbd81296b5af1acdf59a56fe16dee49ec088947e
Beatrice T <be@trice.codes>
2020-11-20 20:16:09 -0800

    Refactored add_to_cart and edit_quantity to be in order_item controller, implemented use of quantity in cart management

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
 
   root to: "homepages#index"
+  # TODO remove unused routes
   resources :homepages
   resources :merchants
   resources :order_items
   resources :orders, except: :show
-  resources :products
+  resources :products, except: :destroy # Called retire for clarity
+  delete '/products/:id', to: 'merchants#retire', as: 'retire_product'
 
   # resources :categories
 
-  resources :products, except: [:destroy, :new, :create] # Called retire for clarity
-  delete '/products/:id', to: 'merchants#retire', as: 'retire_product'
-
+  # Login stuff
   get '/auth/:provider/callback', to: 'merchants#create', as: 'auth_callback'
   get '/auth/github', as: 'github_login'
-  
   delete '/logout', to: 'merchants#destroy', as: 'logout'
+
+  # Cart stuff
   get '/cart', to: 'orders#cart', as: 'cart'
-  patch '/cart/:id', to: 'orders#add_to_cart', as: 'add_cart'
+  patch '/cart/edit_quantity/:id', to: 'order_items#edit_quantity', as: 'edit_quantity'
+  patch '/cart/:id', to: 'order_items#add_to_cart', as: 'add_cart'
 
   # get 'homepages/index'
   # get 'order_items/index'
