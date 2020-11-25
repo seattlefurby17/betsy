efc6f5f616664846b529847dd35af58f6954f316
Sandy <sandyvasquez9@gmail.com>
2020-11-24 17:31:50 -0800

    Added test framework for order items

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -5,5 +5,44 @@ describe OrderItemsController do
   #   get order_items_index_url
   #   must_respond_with :success
   # end
+  #
+  it "can save an order" do
 
+
+  end
+
+  it "can add to an existing order" do
+
+  end
+
+  it "can retire a product and redirect to products path" do
+
+
+
+    must_respond_with :redirect
+    expect(flash[:error]).must_equal "That product is retired!"
+  end
+
+  it "can update a product quantity" do
+
+
+
+    expect(flash[:success]).must_equal "Quantity updated!"
+  end
+
+  it "returns error for invalid product quantity " do
+    # Arrange
+
+    # Act
+    # Assert
+    expect(flash[:error]).must_equal "Invalid quantity specified"
+  end
+
+  it "returns error for item/product not found " do
+    # Arrange
+
+    # Act
+    # Assert
+    expect(flash[:error]).must_equal "Item to edit quantity not found"
+    end
 end
